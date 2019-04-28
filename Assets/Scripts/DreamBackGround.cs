efdf983589bfcada61362ea43d5bbddd398d8896
Soeking <great.from.g@gmail.com>
2019-04-28 18:05:35 +0900

    judge first

--- Assets/Scripts/DreamBackGround.cs
+++ Assets/Scripts/DreamBackGround.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Element : MonoBehaviour
+public class DreamBackGround : MonoBehaviour
 {
-    
+    public static float speed = 1f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //settrans
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //deltatime
+        //if(dash)
     }
 }
---
+++ Assets/Scripts/GlobalData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalData 
+{
+    public static bool isFirst = true;
+
+    public static void change()
+    {
+        isFirst = false;
+    }
+}
