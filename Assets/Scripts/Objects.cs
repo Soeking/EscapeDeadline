e115eca07691264b4fabb21aa2013d8681f5bff6
Soeking <great.from.g@gmail.com>
2019-04-29 00:08:52 +0900

    instantiate

--- Assets/Scripts/Objects.cs
+++ Assets/Scripts/Objects.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Objects : MonoBehaviour
 {
+    public GameObject carObj;
+    public GameObject catObj;
+    public GameObject enemyObj;
+    public GameObject redObj;
+    public GameObject monsterObj;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,11 +18,67 @@ public class Objects : MonoBehaviour
             SetObstacles.firstAddLine();
             GlobalData.change();
         }
+
+        for (int i = 5; i < 10; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                switch (SetObstacles.obstacles[i][j])
+                {
+                    case "car":
+                        Instantiate(carObj, new Vector3((i - 5) * 2, j - 4, 0), Quaternion.identity);
+                        break;
+                    case "cat":
+                        Instantiate(catObj, new Vector3((i - 5) * 2, j - 4, 0), Quaternion.identity);
+                        break;
+                    case "enemy":
+                        Instantiate(enemyObj, new Vector3((i - 5) * 2, j - 4, 0), Quaternion.identity);
+                        break;
+                    case "red":
+                        Instantiate(redObj, new Vector3((i - 5) * 2, j - 4, 0), Quaternion.identity);
+                        break;
+                    case "monster":
+                        Instantiate(monsterObj, new Vector3((i - 5) * 2, j - 4, 0), Quaternion.identity);
+                        break;
+                    case "none":
+                        break;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //BackGround no speed bun idou
+        //if idoukyori > x addLine
+    }
+
+    void addNewLine()
+    {
+        string[] newLine = SetObstacles.addLine();
+        for (int i = 0; i < 5; i++)
+        {
+            switch (newLine[i])
+            {
+                case "car":
+                    Instantiate(carObj, new Vector3(10, i - 4, 0), Quaternion.identity);
+                    break;
+                case "cat":
+                    Instantiate(catObj, new Vector3(10, i - 4, 0), Quaternion.identity);
+                    break;
+                case "enemy":
+                    Instantiate(enemyObj, new Vector3(10, i - 4, 0), Quaternion.identity);
+                    break;
+                case "red":
+                    Instantiate(redObj, new Vector3(10, i - 4, 0), Quaternion.identity);
+                    break;
+                case "monster":
+                    Instantiate(monsterObj, new Vector3(10, i - 4, 0), Quaternion.identity);
+                    break;
+                case "none":
+                    break;
+            }
+        }
     }
 }
--- Assets/Scripts/SetObstacles.cs
+++ Assets/Scripts/SetObstacles.cs
@@ -8,6 +8,7 @@ public class SetObstacles : MonoBehaviour
 {
     public static string[] obstaclesElement = { "car","cat","enemy","red","monster","none","none","none","none","none"};
     public static List<string[]> obstacles = new List<string[]>();
+    public static int range = 10;
     
     // Start is called before the first frame update
     void Start()
@@ -34,21 +35,21 @@ public class SetObstacles : MonoBehaviour
                 }
                 else
                 {
-                    tateLine[i] = obstaclesElement[Random.Range(0, 10)];
+                    tateLine[i] = obstaclesElement[Random.Range(0, range)];
                 }
             }   
             obstacles.Add(tateLine);
         }
     }
     
-    public static void addLine()
+    public static string[] addLine()
     {
         string[] tateLine = new String[5];
         for (int i = 0; i < 5; i++)
         {
-            tateLine[i] = obstaclesElement[Random.Range(0, 10)];
-        }   
-        obstacles.Add(tateLine);
+            tateLine[i] = obstaclesElement[Random.Range(0, range)];
+        } 
+        return tateLine;
     }
 
     public static void deleteLine()
