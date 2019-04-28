f14d723e724e318d0bc12fa738c932f499b06b3e
HarrisonKawagoe <hyzitbios@hotmail.com>
2019-04-28 16:16:56 +0900

    scriptfile2

--- Assets/Scripts/Deadline.cs
+++ Assets/Scripts/Deadline.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Threading.Tasks;
+using System.Timers;
 public class Deadline : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class Deadline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+
+
+
+        Timer timer = new Timer();
+        timer.Interval = 100;
+        timer.Elapsed += (s, e) => pos.y += 1f; myTransform.position = pos;
+        timer.Start();
+
+
+
     }
+
+
 }
--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Human : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        float dimensionX=0;
-        float dimensionY=0;
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += 1f;
+            myTransform.position = pos;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= 1f;
+            myTransform.position = pos;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= 1f;
+            myTransform.position = pos;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += 1f;
+            myTransform.position = pos;
+        }
     }
 }
