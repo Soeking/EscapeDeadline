25ab772506f790aae6bffb9b0359e6a2ef03f8c8
unknown <yuji08752@gmail.com>
2019-04-30 13:07:52 +0900

    miss

---
+++ Assets/Scripts/ChangeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeScript : MonoBehaviour {
+
+  public Sprite spriteDream;
+  public Sprite spriteReal;
+
+  public void chgScript() {
+
+    this.gameObject.GetComponent<Image>().sprite = spriteReal;
+
+  }
+}
